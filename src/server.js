f107312eba1583a3eb2e04ffd7f5aabe7e0dcd26
Muhammad Rehan <muhammadrehan125768@gmail.com>
2024-07-21 11:13:29 +0500

    setup express and build a server in our project

--- src/server.js
+++ src/server.js
@@ -1,4 +1,19 @@
 import "dotenv/config";
 import connectDB from "./db/db.js";
+import app from "./app.js";
+import config from "./config/config.js";
 
-connectDB();
+const connectServer = async () => {
+  try {
+    await connectDB();
+    app.on(`error`, (error) => {
+      console.log(`Error While Connecting To The Server ${error}`);
+    });
+    app.listen(config.PORT || 3000, () => {
+      console.log(`Server is Listen On ${config.PORT} Port`);
+    });
+  } catch (error) {
+    console.log(`Error While Connecting Server Please Wait... ${error}`);
+  }
+};
+connectServer();
