8ad07ae8dc85546f57772ba5c8bf20b00bb68afa
Muhammad Rehan <muhammadrehan125768@gmail.com>
2024-07-21 11:25:38 +0500

    add handle json data and url data middleware in our project

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 const app = express();
 import cors from "cors";
-import { CORS_OPTIONS } from "./constant.js";
+import { CORS_OPTIONS, JSON_LIMIT, URL_LIMIT } from "./constant.js";
 
 app.use(cors(CORS_OPTIONS));
+app.use(express.static("public"));
+app.use(express.json(JSON_LIMIT));
+app.use(express.urlencoded(URL_LIMIT));
 
 export default app;
--- src/constant.js
+++ src/constant.js
@@ -3,5 +3,13 @@ import config from "./config/config.js";
 const DB_NAME = "yt";
 const CORS_OPTIONS = {
   origin: config.FRONTEND_URL,
+  credentials: true,
 };
-export { DB_NAME, CORS_OPTIONS };
+const JSON_LIMIT = {
+  limit: "16kb",
+};
+const URL_LIMIT = {
+  limit: "16kb",
+  extended: true,
+};
+export { DB_NAME, CORS_OPTIONS, JSON_LIMIT, URL_LIMIT };
