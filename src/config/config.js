117d08bfec859bbf0294ef0a7a682d70c7bbce5c
Muhammad Rehan <muhammadrehan125768@gmail.com>
2024-07-21 13:01:20 +0500

    create jwt token generate methods

--- src/config/config.js
+++ src/config/config.js
@@ -3,6 +3,10 @@ const _config = {
   PORT: process.env.PORT,
   FRONTEND_URL: process.env.FRONTEND_URL,
   ENV: process.env.ENV,
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY,
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+  REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY,
 };
 
 const config = Object.freeze(_config);
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
+import config from "../config/config.js";
+import jwt from "jsonwebtoken";
 const userSchema = new Schema(
   {
     username: {
@@ -55,6 +57,32 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.isComparePassword = async function (password) {
   return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      fullName: this.fullName,
+    },
+    config.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: config.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    config.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: config.REFRESH_TOKEN_EXPIRY,
+    }
+  );
 };
 
 const User = mongoose.model("User", userSchema);
