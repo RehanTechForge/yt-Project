342f0014071ad34441987468e67b07a597be4530
Muhammad Rehan <muhammadrehan125768@gmail.com>
2024-07-21 11:48:54 +0500

    add global async handler for handling our async operation

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (requestHandler) => async (req, res, next) => {
+  try {
+    return await requestHandler(req, res, next);
+  } catch (error) {
+    return res.status(error.status || 500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+// const asyncHandler = (requestHandler) => {
+//   return (req, res, next) => {
+//     Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+//       next(error)
+//     );
+//   };
+// };
+
+export { asyncHandler };
