14951b46405c447e0d36e2b05391d2339af226bd
Muhammad Rehan <muhammadrehan125768@gmail.com>
2024-07-21 12:02:00 +0500

    add api response error handler in our project

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,7 @@ const _config = {
   MONGODB_URI: process.env.MONGODB_URI,
   PORT: process.env.PORT,
   FRONTEND_URL: process.env.FRONTEND_URL,
+  ENV: process.env.ENV,
 };
 
 const config = Object.freeze(_config);
---
+++ src/utils/apiError.js
@@ -0,0 +1,18 @@
+import config from "../config/config.js";
+
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something Went Wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+    config.ENV === "development" ? (this.stack = stack) : (stack = "");
+  }
+}
