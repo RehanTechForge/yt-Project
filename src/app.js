8f1f2002c57d7fcb456be365e13e6ec6b7aad0a7
Muhammad Rehan <muhammadrehan125768@gmail.com>
2024-07-21 11:39:10 +0500

    setup cookie parser as a global middleware

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 const app = express();
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import { CORS_OPTIONS, JSON_LIMIT, URL_LIMIT } from "./constant.js";
 
 app.use(cors(CORS_OPTIONS));
 app.use(express.static("public"));
 app.use(express.json(JSON_LIMIT));
 app.use(express.urlencoded(URL_LIMIT));
+app.use(cookieParser());
 
 export default app;
