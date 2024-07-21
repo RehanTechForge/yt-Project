2f5b4e186dad344443d45540a2f5c0318fa26437
Muhammad Rehan <muhammadrehan125768@gmail.com>
2024-07-21 13:55:09 +0500

    add image/video upload method server to cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,4 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
+import fs from "node:fs";
 import config from "../config/config.js";
 // Configuration
 cloudinary.config({
@@ -6,3 +7,21 @@ cloudinary.config({
   api_key: config.CLOUDINARY_API_KEY,
   api_secret: config.CLOUDINARY_API_SECRET, // Click 'View Credentials' below to copy your API secret
 });
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+      folder: "yt",
+    });
+    if (!response) {
+      return null;
+    }
+    return response;
+  } catch (error) {
+    console.log(`Error While Upload On Cloudinary ${error}`);
+    fs.unlinkSync(localFilePath);
+  }
+};
+
+export { uploadOnCloudinary };
