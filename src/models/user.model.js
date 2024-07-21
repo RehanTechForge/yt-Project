15da79ebd8ab6e844548753312c39e709fe38755
Muhammad Rehan <muhammadrehan125768@gmail.com>
2024-07-21 12:48:31 +0500

    add user password encryption and decryption using bcrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 const userSchema = new Schema(
   {
     username: {
@@ -45,6 +46,16 @@ const userSchema = new Schema(
 
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) next();
+  await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isComparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 export default User;
