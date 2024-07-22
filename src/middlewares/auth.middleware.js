8a71b69ae12d6749e612c29a1d9c88c514d7b7cc
Muhammad Rehan <muhammadrehan125768@gmail.com>
2024-07-22 12:45:37 +0500

    fix auth middleware and remove unwanted logs

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -14,7 +14,7 @@ const verifyJWT = asyncHandler(async (req, res, next) => {
   const decodedToken = jwt.verify(token, config.ACCESS_TOKEN_SECRET);
 
   const user = await User.findById(decodedToken._id).select(
-    "-password refreshToken"
+    "-password -refreshToken"
   );
 
   if (!user) {
